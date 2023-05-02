e830240d0e0851a29a684855e1550a4025115827
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-05-02 10:25:48 +0300

    shape update

--- 101/ders1/openimg.py
+++ 101/ders1/openimg.py
@@ -2,11 +2,16 @@ import cv2
 import numpy as np 
 
 # image = cv2.imread("aiexample.webp")
-image = cv2.imread("aiexample.webp",0) #fotoğrafı gri okur
+image = cv2.imread("101/ders1/aiexample.webp",0) #fotoğrafı gri okur, kanalı 0 okudu
 
 cv2.imshow("ai logo", image)
 
-cv2.imwrite("blackai.webp",image) #yeni fotoğraf oluşturdu
+cv2.imwrite("101/ders1/blackai.webp",image) #yeni fotoğraf oluşturdu
+
+print(image) #görselin piksel ölçülerini matris şeklinde verir
+print(image.size) 
+print(image.dtype) 
+print(image.shape) 
 
 cv2.waitKey(0) #pencere açıldığında pencerenin kalkması için tuşa basmamı bekler
 cv2.destroyAllWindows() #çarpıya basınca opencv kapanır
