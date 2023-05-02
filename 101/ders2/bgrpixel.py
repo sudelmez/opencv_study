295cb355dc19bbe06b8d07398d91bd5b09387fb0
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-05-02 13:26:55 +0300

    lesson 2 update

--- 101/ders2/bgrpixel.py
+++ 101/ders2/bgrpixel.py
@@ -3,9 +3,17 @@ import numpy as np
 
 wolf = cv2.imread("101/ders2/wolf.jpg")
 
-cv2.imshow("kurt",wolf)
+
 print(wolf[(230,80)]) #renk olarak 222 blue,227 green, 230 red değeri geldi
 
+# wolf[50,30]= [255,255,255] #belirtilen konumdaki piksel beyaz renge boyandı
+
+for i in range(100):
+    wolf[50,i]= [255,255,255] #belirtilen alan kadar beyaz boyar 
+
+
+
+cv2.imshow("kurt",wolf)
 
 cv2.waitKey(0) #pencere açıldığında pencerenin kalkması için tuşa basmamı bekler
 cv2.destroyAllWindows() #çarpıya basınca opencv kapanır
